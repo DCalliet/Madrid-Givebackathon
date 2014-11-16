3a3cbd44349c93b2ed6ce095a28ae809c50c3592
Darius Calliet <dcalliet@topoppssoftware.com>
2014-11-15 19:06:27 -0600

    users fixed

---
+++ db_repository/versions/004_migration.py
@@ -0,0 +1,34 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+cause = Table('cause', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+)
+
+contributions = Table('contributions', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('user_id', Integer),
+    Column('date', DateTime),
+    Column('amount', Integer),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['cause'].create()
+    post_meta.tables['contributions'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['cause'].drop()
+    post_meta.tables['contributions'].drop()
